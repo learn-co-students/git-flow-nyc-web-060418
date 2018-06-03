f3d0a18352ade97c3a66a8a814528c0e25de5359
Changmin Lim <chang.lim92@gmail.com>
2018-06-03 08:59:41 +0000

    automatically backed up by learn

--- spec/comprehension_spec.rb
+++ spec/comprehension_spec.rb
@@ -1,4 +1,5 @@
 describe "GIT" do
+  it 
 # describe "STAGING AND COMMITTING CHANGES:" do
   it "1. What is the syntax for staging a change in index.html if it is in the root dir?" do
     answer = "git add index.html"
